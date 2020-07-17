00437c07f8c7361e785377c44cdba4eb571aa12b
Chethan091995 <68340538+Chethan091995@users.noreply.github.com>
2020-07-17 19:54:43 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("hello world")#HttpResponse(HTML tag as an argument)
 
 def home(request):
     return HttpResponse("<h1>Welcome to homepage<h1>")
 
-
+def Demo(request):
+    return render(request,"sample.html")
